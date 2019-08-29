1180e912225313a6bbc3b52f18b8947a23729ea9
Patrick <patrick.yrkim@gmail.com>
2019-08-28 18:47:10 -0700

    Add minesweeper phase 5 and redux_lite

--- minesweeper/components/game.jsx
+++ minesweeper/components/game.jsx
@@ -5,15 +5,44 @@ import * as Minesweeper from '../minsweeper';
 class Game extends React.Component {
     constructor(props) {
         super(props);
-        const board = new Minesweeper.board
+        const board = new Minesweeper.board(9, 10);
         this.state = {board: board};
+        
+        this.updateGame = this.updateGame.bind(this);
+        this.restartGame = this.restartGame.bind(this);
     }
 
-    updateGame() {
+    updateGame(tile, flagged) {
+        if (flagged) {
+            tile.toggleFlag();
+        } else {
+            tile.explore();
+        }
 
+        this.setState({ board: this.state.board });
+    }
+
+    restartGame() {
+        let board = new Minesweeper.board(9, 10);
+
+        this.setState({ board: board });
     }
 
     render() {
+        let modal;
+
+        if (this.state.board.lost() || this.state.board.won()) {
+            let popup = this.state.board.won() ? "You win!" : "You lose!";
+
+            modal = 
+                <div className="modal-screen">
+                    <div className="modal-content">
+                        <p>{popup}</p>
+                        <button onClick={this.restartGame}>Restart</button>
+                    </div>
+                </div>
+        }
+
         return (
             <div>
                 {modal}
---
+++ redux_lite/store.js
@@ -0,0 +1,10 @@
+class Store {
+    constructor(rootReducer) {
+        this.rootReducer = rootReducer;
+        this.state = {};
+    }
+
+    getState() {
+        return Object.assign({}, this.state);
+    }
+}
